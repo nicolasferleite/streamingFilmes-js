94e55084d8b22be31848efbe7b3ba50da1ad0926
Nicolas Ferreira <nicolasferreiraleite@gmail.com>
2024-06-17 21:16:56 -0300

    esta dando erro no salvamento

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,49 @@
-
+import './favoritos.css';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Favoritos(){
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        const minhaLista = localStorage.getItem("@primeflix");
+        
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    }, []);
+
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((filme) => {
+            return(filme.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+    }                                                                                           
+
     return(
-        <div>
-            <h1>Bem-vindo a página de Favoritos</h1>
+        <div className='meus-filmes'>
+            <h1>Meus filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo!</span>}
+
+            <ul>
+                {
+                    filmes.map((filme) => {
+                        return(
+                            <li key={filme.id}>
+                                <span>{filme.title}</span>
+                                <div>
+                                    <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
+                                    <button onClick={() => excluirFilme(filme.id)}>Excluir</button>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -34,7 +34,23 @@ function Filme(){
         return () => {
             console.log("COMPONENTE FOI DESMONTADO");
         }
-    }, [navigate, id])
+    }, [navigate, id]);
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id);
+
+        if(hasFilme){
+            alert("ESSE FILME JÁ ESTÁ SALVO NOS SEUS FAVORITOS!");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        alert("FILME SALVO COM SUCESSO!");
+    }
 
     if(loading){
         return(
@@ -54,9 +70,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a target='_blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
+                    <a target='blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
                 </button>
             </div>
         </div>
